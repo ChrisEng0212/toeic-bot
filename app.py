d3967841f964c01ba40dd749e2f9710379a8858d
ChrisEng0212 <cjx02121981@gmail.com>
2020-06-27 21:32:46 +0800

    print debug

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def callback():
     # handle webhook body
     try:
         handler.handle(body, signature)
+        print('handler success')
     except InvalidSignatureError:
         abort(400)
     return 'OK'
@@ -42,7 +43,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    print(message)
+    print('message', message)
+    print('token', event.reply_token)
     line_bot_api.reply_message(event.reply_token, message)
     
 import os
