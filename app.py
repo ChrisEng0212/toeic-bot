997aeab683ae11983f933f2c3bdd194a45abec82
ChrisEng0212 <cjx02121981@gmail.com>
2020-06-27 21:47:50 +0800

    more print markers

--- app.py
+++ app.py
@@ -24,28 +24,39 @@ def hello_world():
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    print('callback')
+    print('CALLBACK')
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    print(signature)
+    print('SIGNATURE', signature)
     # get request body as text
     body = request.get_data(as_text=True)
+    print('BODY', body)
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
+        print('HANDLER try')
         handler.handle(body, signature)
-        print('handler success')
+        print('HANDLER success')
     except InvalidSignatureError:
         abort(400)
     return 'OK'
+
+'''
+event {"message": {
+    "id": "100001",
+     "text": "Hello, world", "type": "text"},
+      "replyToken": "00000000000000000000000000000000",
+      "source": {"type": "user", "userId": "Udeadbeefdeadbeefdeadbeefdeadbeef"}, 
+      "timestamp": 1593265283364, 
+      "type": "message"
+    }
+'''
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    print('event', event)
-    print('message', message)
-    print('token', event.reply_token)
+    print('EVENT', event)    
     line_bot_api.reply_message(event.reply_token, message)
     
 import os
