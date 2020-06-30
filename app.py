332aa62ea532d790b17dfcf631b947969c93e2bb
ChrisEng0212 <cjx02121981@gmail.com>
2020-06-30 11:37:55 +0800

    flask update

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, request, abort
-import json
-import abc
+from flask import Flask, request, abort, render_template, url_for, flash, redirect, jsonify  
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, current_user, logout_user, login_required
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from datetime import datetime, timedelta
+import json
+import os
+
+try:
+    from meta import SQLALCHEMY_DATABASE_URI, SECRET_KEY, DEBUG, channel_access_token, channel_secret
+
+except:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']    
+    SECRET_KEY = os.environ['SECRET_KEY']    
+    DEBUG = False
+    channel_access_token = os.environ['CHANNEL_ACCESS'] 
+    channel_secret = os.environ['CHANNEL_SECRET'] 
+ 
 
 from linebot import (
     LineBotApi, WebhookHandler, WebhookParser
@@ -12,17 +29,56 @@ from linebot.exceptions import (
 from linebot.models import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['DEBUG'] = DEBUG
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' 
+login_manager.login_message_category = 'info'
 
 # Channel Access Token
-line_bot_api = LineBotApi('HzvSBrRQ11nxA+2sex/gtMWHL0DjzhJZy6VOK/i0aFh1836BK0ifbYJYarNehZgD6xFwlrSU6E9b2f00VaAHyVYOweaLpJLYWuHFTMljLBuszbcEqPEQzaVr/yWQ3Y1tetD6goR3u6FHCNDjDeFzwgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(channel_access_token)
 # Channel Secret
-handler = WebhookHandler('6c1237cad81c1ba197e04f6d179e906e')
+handler = WebhookHandler(channel_secret)
 
-parser = WebhookParser('6c1237cad81c1ba197e04f6d179e906e')
+parser = WebhookParser(channel_secret)
+
+
+class User(db.Model, UserMixin): 
+    id = db.Column(db.Integer, primary_key=True) 
+    username =  db.Column(db.String(20), unique=True, nullable=False)    
+    
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    line = db.Column(db.String)
+    name = db.Column(db.String)
+    highschool = db.Column(db.String)
+    number = db.Column(db.String)
+    dept = db.Column(db.String)
+    questions = db.Column(db.String)
+    
+
+@app.route("/login/<string:pw>", methods=['GET','POST'])
+def login(pw):
+    if pw == 'pw':
+        user = User.query.filter_by(username='Admin').first()
+        login_user (user)
+        flash (f'Debug Login', 'warning') 
+        redirect (url_for('data'))   
+  
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello, World!'
+
+@app.route('/data')
+@login_required
+def data():
+    return 'Data'
+
 
 @app.route('/text/<string:tx>')
 def text(tx):
@@ -35,14 +91,12 @@ def text(tx):
 
 
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     print('CALLBACK')
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     print('SIGNATURE', signature)
-
             
     # get request body as text
     body = request.get_data(as_text=True)
@@ -52,10 +106,9 @@ def callback():
     # parse webhook body
     try:
         events = parser.parse(body, signature)
-        print('event PARSE')
+        print('event PARSE', events)
     except InvalidSignatureError:
         abort(400)
-
 
     # handle webhook body
     try:
@@ -66,12 +119,13 @@ def callback():
         abort(400)
     return 'OK'
 
+
 @handler.add(PostbackEvent)
 def handle_message(event, destination):
-    print('POSTBACK', event.postback.data)
-    print('RepTOK', event.reply_token)
-    print('ID', event.source.user_id) 
-
+    print('POSTBACK', event.postback.data)   
+    
+    if isinstance(event.source, SourceUser):
+        print('ID', event.source.user_id) # user_id replaces userId
     
     sticker_message = StickerSendMessage(
     package_id='1',
@@ -83,18 +137,20 @@ def handle_message(event, destination):
     #print(profile)
 
 
-
 @handler.add(FollowEvent)
-def handle_follow():    
+def handle_follow():  
     line_bot_api.reply_message(event.reply_token, 'welcome')
-
 
 
 @handler.add(UnfollowEvent)
 def handle_follow():        
-    line_bot_api.reply_message(event.reply_token, 'welcome')
+    print('student has left')
 
 
+#If there is no handler for an event, this default handler method is called.
+@handler.default()
+def default(event):
+    print('DEFAULT', event)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):    
